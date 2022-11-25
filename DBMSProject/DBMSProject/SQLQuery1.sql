07434b721722c8616e0009d58ec6b55a84405e40
HonestPhoenix <113068231+Zburgess1@users.noreply.github.com>
2022-11-24 23:10:27 -0600

    Fixed this bad boy

---
+++ DBMSProject/DBMSProject/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO UserReviews (ISBN, Review)
+VALUES ('918-3-05-148410-7', 'Unoriginal, would not recommend'),
+('950-3-11-148410-0', 'Completely sadistic... Would recommend'),
+('977-3-16-148410-1', 'www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html'),
+('978-3-16-148410-0', 'Dale did wander. Dale did not disappoint'),
+('978-3-16-342417-0', 'Complete copy of Iron Man. RDJ was not enthused.'),
+('978-5-16-128410-6', 'You know who else has their own book?'),
+('998-3-16-248510-0', 'Them platypuses were rafting...');
